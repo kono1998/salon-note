940e075452405ecfbd8848047689486ff6fc267a
Konomi Tomonishi <konomi.0425.usa@gmail.com>
2026-09-07 12:05:27 +0000

    特定商取引法ページ(LegalNotice)をローカルに統合し、/legal ルートを復元

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ConfirmPage from './ConfirmPage.jsx'
+import LegalNotice from './LegalNotice.jsx'
 import './index.css'
 
 // /confirmed パスのときだけ認証完了ページを表示
 const isConfirmPage = window.location.pathname === '/confirmed'
+// /legal パスのときは特定商取引法に基づく表記を表示
+const isLegalPage = window.location.pathname === '/legal'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isConfirmPage ? <ConfirmPage /> : <App />}
+    {isConfirmPage ? <ConfirmPage /> : isLegalPage ? <LegalNotice /> : <App />}
   </React.StrictMode>,
 )
